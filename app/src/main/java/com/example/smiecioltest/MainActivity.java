b5cf6bed74b1ae9d9ae1147aa3398976f78fd7af
urbanpaulina <paulinaurban891@gmail.com>
2021-01-06 16:46:38 +0100

    user-my profile

--- app/src/main/java/com/example/smiecioltest/MainActivity.java
+++ app/src/main/java/com/example/smiecioltest/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.smiecioltest;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,25 +12,8 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
 
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Switch;
-import android.widget.Toast;
-
-import com.example.smiecioltest.model.User;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.FirebaseFirestore;
 
 
 
@@ -54,8 +41,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction().replace(R.id.Fragment_container,
-                    new statystyki_fragment()).commit();
-            navigationView.setCheckedItem(R.id.statystyki);
+                    new MyProfile_fragment()).commit();
+            navigationView.setCheckedItem(R.id.my_profile);
         }
         onas_ragment onas_fragmentt = new onas_ragment();
         FragmentManager fm = getSupportFragmentManager();
@@ -66,6 +53,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
         switch (menuItem.getItemId()){
+            case R.id.my_profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.Fragment_container,new MyProfile_fragment()).commit();
+                break;
             case R.id.statystyki:
                 getSupportFragmentManager().beginTransaction().replace(R.id.Fragment_container,new statystyki_fragment()).commit();
                 break;
